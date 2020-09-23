2a4ffef0ff6112864165aef26b268d3a984763fd
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-23 03:16:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
 
-  name_hash.each do |key, value|
-    if key[value] < key[value].max
-      key_val = key
+  name_hash.each do |arg, value|
+    if arg[value] < arg[value].max
+      key_val = arg
     end
   end
     key_val
