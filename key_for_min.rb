a3a6a66997920202a4fd6354f6c1893205cdb311
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-23 03:28:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@
 def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = Float::INFINITY
-  hash.each do |k, v|
-    if v < lowest_value
-      lowest_value = v
-      lowest_key = k
+  hash.each do |a, b|
+    if a < lowest_value
+      lowest_value = b
+      lowest_key = a
     end
   end
   lowest_key
